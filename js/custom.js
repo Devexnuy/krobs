af0bed4366c88cb1ee9b1493f8469dafb4cc732d
Fernando López <fernando@developerlopez.com>
2016-12-15 02:51:01 -0600

    Add custom navigation with slider

--- js/custom.js
+++ js/custom.js
@@ -1,4 +1,44 @@
 jQuery(document).ready(function($) {
-    // Start news instance of swiper.js for main menu.
-    
+    var myElem = document.getElementById('SW_master');
+    if (myElem !== null) {
+        var a = new Swiper(".swiper-container");
+        var z = document.getElementById("SW_master");
+        if(z !== null) {
+            z = z.childElementCount;
+            a.slideTo(0);
+        }
+        // Add handler that will be executed only once
+        a.on('slideChangeStart', function () {
+            jQuery(function($){
+                if ( $( "#SW_master" ).length ) {
+                    $('#SW_master .swiper-slide-active').append( '<span class="load-more" style="clear: both;display: block"></span>' );
+                    var id_loop = $('#SW_master .swiper-slide-active #id_loop').html();
+                    localStorage.setItem(id_loop, "2");
+                    localStorage.setItem("loop_active", id_loop);
+                }
+            });
+        });
+        // Menu custom click for categories
+        var categories = [
+            'en-portada',
+            'sucesos-en-honduras',
+            'homepage-sport',
+            'otros-deportes',
+            'noticias-internacionales',
+            'hondurenos-por-el-mundo',
+            'opinion',
+            'opiniones-tiempo',
+            'periodismo-ciudadanos-honduras',
+            'autos'
+        ];
+        $('.menu-item').click(function(e){
+            for (var i = 0; i < categories.length; i++) {
+                var url = $(this).children('a').attr('href');
+                if (url.indexOf(categories[i]) >= 0) {
+                    e.preventDefault();
+                    a.slideTo(i);
+                }
+            }
+        });
+    }
 });
